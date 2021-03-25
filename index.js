833dfbb392bfe464dbee6bb439a6de2a5b45b66a
Abhiramakc <66126677+Abhiramakc@users.noreply.github.com>
2021-03-25 08:28:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2) {
+
+  document.querySelector("h1").textContent = "Player 1 wins";
+
+} else if(randomNumber2 === randomNumber1) {
+
+  document.querySelector("h1").textContent = "Draw";
+
+} else {
+
+  document.querySelector("h1").textContent = "Player 2 wins";
+  
+}
